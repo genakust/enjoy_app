83360adaf0724b92f6211fbd700fe4e92f5d2ef9
Gena <genakust@yahoo.de>
2020-08-26 19:11:54 +0200

    fixed a bug

--- lib/screens/eye_exercises.dart
+++ lib/screens/eye_exercises.dart
@@ -3,10 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:enjoy_app/widgets/bottom_nav_bar.dart';
 import 'package:enjoy_app/widgets/circle.dart';
 
-class EyeExersises extends StatelessWidget {
-  final size = MediaQuery.of(context).size;
+class EyeExersises extends StatefulWidget {
+  @override
+  _EyeExersises_State createState() => _EyeExersises_State();
+}
+
+class _EyeExersises_State extends State<EyeExersises> {
+  static const String id = 'eye_exersises';
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       body: Column(
         children: <Widget>[
